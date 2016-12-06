e500d3ba955aeb82dd880d299583c4d894b6dde9
TheVikingGent <alexander.brandborg@hotmail.com>
2016-12-06 21:29:32 +0100

    Initial_config_maker

--- framework/experiments/brandborg.py
+++ framework/experiments/brandborg.py
@@ -3,6 +3,54 @@ from recipe import Recipe
 from configuration.tabu_search import tabu_search, get_travsersal_info
 
 
+def get_top_nodes(G):
+     top_nodes = []
+     for node in G:
+          no_preds = True
+          predecessors = G.predecessors_iter(node)
+
+          for n in predecessors:
+               no_preds = False
+               break
+
+          if no_preds:
+               top_nodes.append(node)
+
+     return top_nodes
+
+
+def initial_configurations(G, modules, setup):
+    G_copy = G.copy()
+    top_nodes = []
+
+    # If a setup is already given, remove as many top nodes as possible
+    if setup:
+        flag = True  # Flag tells if we may remove more top nodes
+        while flag:
+            flag = False
+            top_nodes = get_top_nodes(G_copy)
+            for node in top_nodes:
+                if node in setup[-1].w_type:
+                    G_copy.remove_node(node)
+                    flag = True
+    # If setup is empty just get the top nodes
+    else:
+        top_nodes = get_top_nodes(G_copy)
+
+    results = []
+    if not G_copy:
+        results.append(setup)
+    else:
+        for node in top_nodes:
+            mods = [m for m in modules if node in m.w_type]
+            for m in mods:
+                update_mods = modules.copy()
+                update_mods.remove(m)
+                new_setup = setup + [m]
+                results = results + (initial_configurations(G_copy, update_mods, new_setup))
+    return results
+
+
 t = [[0, 0, 0, 0],
      [0, 0, 0, 0],
      [0, 0, 0, 0],
@@ -27,9 +75,14 @@ m1.up = m2
 
 r0 = Recipe('chokolade', {'hammer': set(), 'mere hammer': {'hammer'}, 'skrue': {'mere hammer'}, 'pakke': {'skrue'}}, 'hammer-maskine', 0, 2)
 
+G = r0.to_DiGraph().reverse()
+print(initial_configurations(G, [m0,m1,m2, m3, m4], []))
 
-res = tabu_search([r0], [m0, m1, m2, m3, m4], (lambda r, m, t: [m0, m1, m2]))
-print(res)
+
+
+
+#res = tabu_search([r0], [m0, m1, m2, m3, m4], (lambda r, m, t: [m0, m1, m2]))
+#print(res)
 
 
 #worked_on, transported_through = get_travsersal_info("../experiments/Output.txt", m_map, r_map)
