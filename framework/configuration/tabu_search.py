a12661efdd0d28fc8c943ad4cfc9b75114d1f95f
Mathias Jensen <betaboxguugi@gmail.com>
2016-12-06 10:48:07 +0100

    refactored Tabu Search

--- framework/configuration/tabu_search.py
+++ framework/configuration/tabu_search.py
@@ -7,70 +7,83 @@ XML_TEMPLATE = "../../Modeler/iter3.4.1.xml"
 
 
 def tabu_search(recipes, modules, init_func, iters=50):
-    """
-    :param recipes:
-    :param modules:
-    :param init_func:
-    :return:
+    """ Tabu Search
+    :param recipes: A list of Recipe objects
+    :param modules: A list of module objects
+    :param init_func: A function that creates the initial configuration
+    :param iters: How many iterations of Tabu search
+    :return: The best configuration found by the search
     """
     def get_neighbours():
         """ Gets the neighbours of the current frontier.
         :return: A list of tuples, where the first element of the tuple is a string representing a configuration and
         the second element its evaluation.
         """
-        return neighbours_func(swap_neighbours, frontier, recipes, short_term_memory)
+        return neighbours_func(neighbours_swap, frontier, recipes, dynamic_memory)
 
-    init_modules = init_func(recipes, modules, None)
-    free_modules = [m for m in modules if m not in init_modules]
-    
-    init_time = get_best_time(recipes, init_modules, XML_TEMPLATE, VERIFYTA)
+    # Memory used for remembering evalutations, used so we dont have to evaluate the same configuration twice.
+    dynamic_memory = {}
 
-    current_best = (SquareModule.configuration_str(init_modules[0]), init_time)
-    frontier = (SquareModule.configuration_str(init_modules[0]), init_time)
-
+    # Tabu Search specific memories
     long_term_memory = []
     intermediate_memory = []
     short_term_memory = []
 
-    for i in range(iters):
+    # Creating the initial configuration and evalutates it
+    init_modules = init_func(recipes, modules, None)
+    init_config = SquareModule.configuration_str(init_modules[0])
+    init_time = get_best_time(recipes, init_modules, XML_TEMPLATE, VERIFYTA)
+    dynamic_memory[init_config] = init_time
+
+    overall_best = (init_config, init_time)
+    frontier = (init_config, init_time)
+
+    # Here begins the actual search
+    for _ in range(iters):  # TODO: Maybe have stopping criteria instead of iterations, or allow for both.
+        # TODO: Actually start doing Tabu like search stuff here, i.e. use memories, backtrace and shit.
         neighbours = get_neighbours()
-        short_term_memory += neighbours
-        best = min(neighbours, key=(lambda x: x[1]))
-        if best[1] < current_best[1]:
-            current_best = best
+        neighbour_evaluations = [(config, dynamic_memory[config]) for config in neighbours]
+        best = min(neighbour_evaluations, key=(lambda x: x[1])) # Finds the now best neighbour
+        if best[1] < overall_best[1]:
+            overall_best = best
         frontier = best
 
-    return current_best
+    return overall_best
 
-def neighbours_func(get_neighbours_func, frontier, recipes, short_term_memory):
-    """
-    :param foo:
-    :param frontier:
-    :param recipes:
-    :return:
+
+def neighbours_func(get_neighbours_func, frontier, recipes, dynamic_memory):
+    """ High order function that does some stuff that all neighbour finding functions need to do, such as evaluating
+    the neighbours.
+    :param get_neighbours_func: The function that actually finds neighbours. Must take a frontier and recipes as
+    arguments
+    :param frontier: The config that the tabu search is currently finding neighbours for
+    :param recipes: A list of Recipe objects
+    :param dynamic_memory: A dictionary of config, evalutation pairs. Here the evaluations of the neighbours are saved
+    and it is also used so that we don't evaluate the same configuration twice.
+    :return: A list of configurations, that is the neighbours.
     """
     SquareModule.make_configuration(frontier[0])
     neighbours = get_neighbours_func(frontier, recipes)
-    neighbours = [config for config in neighbours if config not in short_term_memory]
-    result = []
-    for config in neighbours:
-        SquareModule.make_configuration(config)
+    neighbours_to_eval = [config for config in neighbours if config not in dynamic_memory]
+    for config in neighbours_to_eval:
+        SquareModule.make_configuration(config)             # SIDE EFFECT: Makes loads of changes to modules
         modules = SquareModule.modules_in_config(config)
         evaluation = get_best_time(recipes, modules, XML_TEMPLATE, VERIFYTA)
-        result.append((config, evaluation))
+        dynamic_memory[config] = evaluation                 # SIDE EFFECT: Makes changes to the dynamic_memory argument
+    return neighbours
 
-    return result
 
-def swap_neighbours(frontier, recipes):
-    """
-    :param config_str:
+def neighbours_swap(frontier, recipes):
+    """ Finds all neighbours where we can swap modules out, but still retain the same active works
+    :param frontier: The config that the tabu search is currently finding neighbours for
+    :param recipes: A list of Recipe objects
     :return:
     """
     def swap(old, new):
-        """
+        """ Does the actual swap in the configuration
         :param old: The modules you wish to swap out
         :param new: The module that you wish to swap in
-        :return:
+        :return: A new configuration string, where we swapped old with new
         """
         config_str = SquareModule.configuration_str(old)
         l = config_str.split(sep=':')
@@ -90,28 +103,14 @@ def swap_neighbours(frontier, recipes):
         new_config_str = ':'.join(l)
         return new_config_str
 
-    def swappable_modules(config_module, free_modules):
-        """
-        :param config_module:
-        :param free_modules:
-        :return:
-        """
-        L = []
-        for fm in free_modules:
-            if config_module.active_w_type <= fm.w_type:
-                L.append(fm)
-        return L
-
     config_str = frontier[0]
     config_modules = SquareModule.modules_in_config(config_str)
     free_modules = SquareModule.modules_not_in_config(config_str)
 
     neighbours = []
-
-    for config_module in config_modules:
-        swappable = [fm for fm in free_modules if config_module.active_w_type <= fm.w_type]
-        for free_m in swappable:
-            neighbours.append(swap(config_module, free_m))
-
+    for old in config_modules:
+        swappable = [new for new in free_modules if old.active_w_type <= new.w_type]
+        for new in swappable:
+            neighbours.append(swap(old, new))
     return neighbours
 
